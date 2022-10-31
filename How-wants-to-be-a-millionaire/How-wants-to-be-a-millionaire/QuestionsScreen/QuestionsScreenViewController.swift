9a62ead067ca2121aed1a062d1a9dccd97482f21
Dariia <dariia.grigoreva@gmail.com>
2022-10-31 23:51:02 +0300

    add iboutlet

--- How-wants-to-be-a-millionaire/How-wants-to-be-a-millionaire/QuestionsScreen/QuestionsScreenViewController.swift
+++ How-wants-to-be-a-millionaire/How-wants-to-be-a-millionaire/QuestionsScreen/QuestionsScreenViewController.swift
@@ -9,25 +9,49 @@ import UIKit
 
 class QuestionsScreenViewController: UIViewController {
     
+    @IBOutlet weak var buttonAView: UIView!
+    @IBOutlet weak var buttonBView: UIView!
+    @IBOutlet weak var buttonCView: UIView!
+    @IBOutlet weak var buttonDView: UIView!
+    
+    @IBOutlet weak var questionTextLabel: UILabel!
+    @IBOutlet weak var questionNumberLabel: UILabel!
+    @IBOutlet weak var cashLabel: UILabel!
+    
+    @IBOutlet weak var makeAMistakeImageView: UIImageView!
+    @IBOutlet weak var askTheAudienceImageView: UIImageView!
+    @IBOutlet weak var fiftyFiftyImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
-//        button.addGestureRecognizer(tap)
-
+        
+        for image in [makeAMistakeImageView, askTheAudienceImageView, fiftyFiftyImageView] {
+            let tap = UITapGestureRecognizer(target: self, action: #selector(imageViewDidTapped(_:)))
+            image?.isUserInteractionEnabled = true
+            image?.addGestureRecognizer(tap)
+        }
+        
+        for button in [buttonAView, buttonBView, buttonCView, buttonDView] {
+            let tap = UITapGestureRecognizer(target: self, action: #selector(buttonDidTapped(_:)))
+            button?.addGestureRecognizer(tap)
+        }
     }
-
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        for button in buttonsViewCollection {
-            button.clipsToBounds = true
-            button.layer.cornerRadius = 20
+        for button in [buttonAView, buttonBView, buttonCView, buttonDView] {
+            button?.clipsToBounds = true
+            button?.layer.cornerRadius = 20
         }
         
     }
     
-    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+    @objc private func imageViewDidTapped(_ sender: UITapGestureRecognizer) {
+        
+    }
+    
+    @objc private func buttonDidTapped(_ sender: UITapGestureRecognizer) {
         
     }
 }
