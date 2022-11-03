1d0b836506c50e5cf8c0db807fb837ae94da991d
Dariia <dariia.grigoreva@gmail.com>
2022-11-03 10:16:44 +0300

    fix 2

---
+++ How-wants-to-be-a-millionaire/How-wants-to-be-a-millionaire/LooseVC/LooseVC.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class LooseVC: UIViewController {
+
+    @IBOutlet weak var attemptLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    @IBAction func playAgainPressed(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
