7fe37d94dccc20cf8e05ae16c6c554cbd79c6255
zcredi <zcredi@gmail.com>
2022-11-03 09:43:29 +0300

    Главная страница

--- How-wants-to-be-a-millionaire/How-wants-to-be-a-millionaire/RulesPage/RulesPage.swift
+++ How-wants-to-be-a-millionaire/How-wants-to-be-a-millionaire/RulesPage/RulesPage.swift
@@ -1,27 +1,20 @@
 //
-//  MainScreen.swift
+//  RulesPage.swift
 //  How-wants-to-be-a-millionaire
 //
-//  Created by Владислав on 31.10.2022.
+//  Created by Владислав on 01.11.2022.
 //
 
 import UIKit
 
-class MainScreen: UIViewController {
-    
-    @IBOutlet weak var rulesButton: UIButton!
-    
-    
+class RulesPage: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func gameRules(sender: AnyObject) {
-        rulesButton.titleLabel?.font = UIFont(name: "Roboto Regular", size: 32)
-        let rulesPage = RulesPage(nibName: "RulesPage", bundle: nil)
-        show(rulesPage, sender: nil)
-    }
 
     /*
     // MARK: - Navigation
