353ffed4cea36a9d0de7bd11609a4fe272717742
Veronica Rudiuk <veronicarudiuk@gmail.com>
2022-11-03 23:38:44 +0400

    add logo, launchScreen and part of buttons appearance logic

--- How-wants-to-be-a-millionaire/How-wants-to-be-a-millionaire/QuestionListVC/QuestionListVC.swift
+++ How-wants-to-be-a-millionaire/How-wants-to-be-a-millionaire/QuestionListVC/QuestionListVC.swift
@@ -16,7 +16,6 @@ class QuestionListVC: UIViewController {
     
     var millionaireBrain = MillionaireBrain()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,7 +28,7 @@ class QuestionListVC: UIViewController {
         }
         
         //        делаю активной актуальную кнопку
-        makeActualQuestionButtonActive(actualQuestionNumber: millionaireBrain.questionNumber + 1)
+        makeActualQuestionButtonActive(actualQuestionNumber: millionaireBrain.questionNumber)
         
     }
     
@@ -42,7 +41,7 @@ class QuestionListVC: UIViewController {
     
     @IBAction func questionButton(_ sender: UIButton) {
         
-//        перехожу на страницу с вопросами
+        //        перехожу на страницу с вопросами
         let questionsScreenViewController = QuestionsScreenViewController(nibName: "QuestionsScreenViewController", bundle: nil)
         questionsScreenViewController.millionaireBrain = millionaireBrain
         show(questionsScreenViewController, sender: nil)
@@ -50,19 +49,39 @@ class QuestionListVC: UIViewController {
     
     //        делаю активной актуальную кнопку
     func makeActualQuestionButtonActive (actualQuestionNumber: Int) {
-        let activeButton = view.viewWithTag(actualQuestionNumber) as! UIButton
-        activeButton.isEnabled = true
-    }
-    
-    //    меняю левый констрейнт у стеквью при повороте экрана
-    func setLeftConstraintForMainStackView() {
-        if UIDevice.current.orientation.isLandscape {
-            mainLeadingConstraint.priority = UILayoutPriority(rawValue: 750)
-            secondLeadingConstraint.priority = UILayoutPriority(rawValue: 999)
-        } else if UIDevice.current.orientation.isPortrait {
-            mainLeadingConstraint.priority = UILayoutPriority(rawValue: 1000)
-            secondLeadingConstraint.priority = UILayoutPriority(rawValue: 750)
+        if actualQuestionNumber > 1 {
+                        let previousQuestionButton = view.viewWithTag(actualQuestionNumber) as! UIButton
+                        previousQuestionButton.setImage(UIImage(named: "GreenButtonBackground"), for: .normal)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.9) {
+                            let activeButton = self.view.viewWithTag(actualQuestionNumber + 1) as! UIButton
+                            activeButton.isEnabled = true
+                            previousQuestionButton.setImage(UIImage(named: "PurpleButtonBackground"), for: .normal)
+                        }
+                    } else {
+                        let activeButton = self.view.viewWithTag(actualQuestionNumber + 1) as! UIButton
+                        activeButton.isEnabled = true
+                    }
+//            UIView.animate(withDuration: 6, delay: 0) {
+//
+//                let activeButton = self.view.viewWithTag(actualQuestionNumber + 1) as! UIButton
+//                activeButton.isEnabled = true
+//                self.view.layoutSubviews()
+//            }
+            
         }
-    }
     
-}
+        
+        //    меняю левый констрейнт у стеквью при повороте экрана
+        func setLeftConstraintForMainStackView() {
+            if UIDevice.current.orientation.isLandscape {
+                mainLeadingConstraint.priority = UILayoutPriority(rawValue: 750)
+                secondLeadingConstraint.priority = UILayoutPriority(rawValue: 999)
+            } else if UIDevice.current.orientation.isPortrait {
+                mainLeadingConstraint.priority = UILayoutPriority(rawValue: 1000)
+                secondLeadingConstraint.priority = UILayoutPriority(rawValue: 750)
+            }
+        }
+        
+        //    func switchingToTheActiveButton
+        
+    }
